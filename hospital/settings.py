c35e5776ea5820e50edf07876b4964d5e3312154
Astrothunder001 <olorunfemidaniel98@gmail.com>
2024-11-20 20:51:40 -0800

    new changes

--- hospital/settings.py
+++ hospital/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bs4h0###5o^5(ma&em2-fr$j73e8+&*8hcskje-@&xmas_(mlm'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY','fallback=secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
